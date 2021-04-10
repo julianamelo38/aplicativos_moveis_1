d2ac4e81e474ed225be7ee00f5279ddf93dd35bc
Juliana Melo <juliana.santos870@etec.sp.gov.br>
2021-04-10 20:36:34 -0300

    ATIVIDADE DE PROGRAMAÇÃO DE APLICATIVOS MÓVEIS-1

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+//import React from 'react'
+//import { View, Text } from 'react-native'
+
+//const Header = (props) => (
+
+   // <View>
+
+     // <Text style={styles.title}>Ecola Etec de Barueri</Text>
+     // <Text style={styles.title}>Curso de Desenvolvimento de Sistema</Text>
+     // <Text style={styles.title}>Juliana de Melo Santos</Text>
+
+  //  </View>
+
+
+//)
+
+
+//const styles= StyleSheet.create({
+  //container:{
+
+    //marginTop:0,
+    //backgroundColor: '#6ca2f7',
+    //alignItems: 'center',
+    //justifyContent: 'center'
+
+  //}
+
+
+
+
+//})
+
+//export default Header
